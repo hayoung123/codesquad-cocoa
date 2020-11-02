2eb924a2f890b7a58adda6c15d38b33a579dd9f5
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-11-02 17:58:25 +0900

    다각형 넓이  구하기 & debugging 기술문서

---
+++ 1102/debugging 기술문서.md
@@ -0,0 +1,13 @@
+# VScode에서 디버깅
+
+## breakpoints
+
+코드 실행 중 내가 stop 해보고 싶은 point.
+
+vsc에서는 코드라인 왼쪽 공백을 클릭하면 빨간색점으로 breakpoint가 설정된다.
+
+breakpoints를 설정하고 debugging하면 break된 상황에 local,global various를 볼 수 있다.
+
+## watch 사용법
+
+## callstack
---
+++ 1102/index.js
@@ -0,0 +1,59 @@
+//다각형 넓이 구하기
+
+function getCircle(radius, maxRadius = radius) {
+  //   if (arguments.length === 0) return console.log("need a radius");
+  //   if (radius > maxRadius) return console.log("error");
+  const PI = Math.PI;
+  let sumArea = 0;
+  while (radius <= maxRadius) {
+    let area = PI * Math.pow(radius, 2);
+    sumArea += area;
+    radius++;
+  }
+  executionSequence.push(`circle:${sumArea}`);
+  return console.log(`total circle area : ${sumArea}`);
+}
+
+function getRect(row, column) {
+  //   if (row === undefined || column === undefined)
+  //     return console.log("need row or column");
+  const area = row * column;
+  console.log(`rectangle area : ${area}`);
+  executionSequence.push(`rect:${area}`);
+  return area;
+}
+
+function getTrapezoid(upperRow, lowerRow, column) {
+  //   if (upperRow === undefined || lowerRow === undefined || column === undefined)
+  //     return console.log("need row or column");
+  const area = ((upperRow + lowerRow) * column) / 2;
+  console.log(`trapezoid area : ${area}`);
+  executionSequence.push(`trapezoid:${area}`);
+  return area;
+}
+
+function getArea(shape, num1, num2, num3) {
+  if (shape === undefined) return console.log("need shape");
+  if (shape === "circle") {
+    getCircle(num1, num2);
+  } else if (shape === "rect") {
+    getRect(num1, num2);
+  } else if (shape === "trapezoid") {
+    getTrapezoid(num1, num2, num3);
+  }
+}
+
+let executionSequence = [];
+function printExecutionSequence() {
+  console.log(`계산수행 : ${executionSequence.join(", ")}`);
+}
+
+function init() {
+  getCircle();
+  getArea("circle", 10);
+  getArea("rect", 10, 15);
+  getArea("trapezoid", 10, 15, 12);
+  getArea("circle", 1, 2);
+  printExecutionSequence();
+}
+init();
