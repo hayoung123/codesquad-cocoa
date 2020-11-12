27de12651d01b3b15580109fc9fdedf589dcba9c
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-11-13 00:57:33 +0900

    make parser func / string,object 포함

--- Day5/token,lexer,parse/dataTree.js
+++ Day5/token,lexer,parse/dataTree.js
@@ -5,9 +5,15 @@ const [L_BRACKET, R_BRAKET, L_BRACE, R_BRACE, COMMA, BLANK] = [
   "}",
   ",",
 ];
+function Node(type, value) {
+  this.type = type;
+  this.value = value;
+  this.child = [];
+}
 function DataTree(data) {
   this.data = data;
   this.tokenArr = [];
+  this.parsedData = new Node("root");
 }
 
 //배열, 객체, 스트링, 숫자 쪼개기
@@ -23,7 +29,10 @@ DataTree.prototype.tokenize = function (stringData) {
       this.tokenArr.push(x);
     } else temp.push(x);
   }
-  return this.tokenArr.filter((v) => !this.isComma(v)).map((v) => v.trim());
+  this.tokenArr = this.tokenArr
+    .filter((v) => !this.isComma(v))
+    .map((v) => v.trim());
+  return this.tokenArr;
 };
 
 DataTree.prototype.isLBraket = function (char) {
@@ -36,6 +45,35 @@ DataTree.prototype.isComma = function (char) {
   return char === COMMA;
 };
 
+DataTree.prototype.parser = function (
+  node = this.parsedData,
+  head = this.parsedData
+) {
+  if (this.tokenArr.length === 0) return head;
+  if (this.isLBraket(this.tokenArr[0])) {
+    const newNode = new Node(
+      this.tokenArr[0] === L_BRACKET ? "array" : "object"
+    );
+    this.tokenArr.shift();
+    this.parser(newNode, node);
+    this.parser(node, head);
+  } else if (this.isRBraket(this.tokenArr[0])) {
+    this.tokenArr.shift();
+    head.child.push(node);
+    return;
+  } else {
+    const newNode = new Node(
+      !isNaN(Number(this.tokenArr[0])) ? "number" : "string",
+      this.tokenArr[0]
+    );
+    this.tokenArr.shift();
+    node.child.push(newNode);
+    this.parser(node, head);
+  }
+};
+
 let a = new DataTree();
 let k = a.tokenize("[12, 23, [hello, [3], world], { x: 3}]");
 console.log(k);
+a.parser();
+console.log(JSON.stringify(a.parsedData, null, "\t"));
