4738bc565603b97285d3eb7c68da821e9f7f36d0
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-11-20 15:57:24 +0900

    doc. event capture & bubbling

--- Day7/이벤트 전파/test.js
+++ Day7/이벤트 전파/test.js
@@ -1,12 +1,11 @@
-const ul = document.querySelector("ul");
-const divs = document.querySelectorAll("div");
-
-// divs.forEach((v) => v.addEventListener("click", handleClick, true));
-document.body.addEventListener("click", handleClick);
-divs.forEach(function (div) {
-  div.addEventListener("click", handleClick, false);
+const elems = document.querySelectorAll("*");
+elems.forEach((v) => {
+  v.addEventListener("click", handleClick, true);
+  v.addEventListener("click", handleClick);
 });
-// document.addEventListener("click", handleClick);
+const first = document.querySelector(".first");
+first.removeEventListener("click", handleClick);
+first.removeEventListener("click", handleClick, true);
 let i = 1;
 function handleClick(event) {
   console.log(`이벤트 발생 순서: ${i++}`);
