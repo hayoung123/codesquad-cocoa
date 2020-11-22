1ee8751d66c089987fc4853331312f1a59634c65
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-11-23 00:40:24 +0900

    update. algorithm logic Grading-Student

--- Day9/Grading-Student.js
+++ Day9/Grading-Student.js
@@ -1,15 +1,7 @@
 function gradingStudents(grades) {
   for (let i = 0; i < grades.length; i++) {
     if (grades[i] < 38) continue;
-    const multi = roundMultiple(5, grades[i]);
-    if (multi - grades[i] < 3) grades[i] = multi;
+    if (grades[i] % 5 >= 3) grades[i] = grades[i] + (5 - (grades[i] % 5));
   }
   return grades;
-}
-function roundMultiple(multi, grade) {
-  let res = 0;
-  while (res <= grade) {
-    res += multi;
-  }
-  return res;
 }
