29f7116ed9dca66ea9bf2c41cdb5526f5a25f7d9
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-11-23 00:32:33 +0900

    doc. event prevent, stop propagation

--- Day7/이벤트 전파/test.js
+++ Day7/이벤트 전파/test.js
@@ -1,6 +1,6 @@
 const elems = document.querySelectorAll("*");
 elems.forEach((v) => {
-  v.addEventListener("click", handleClick, true);
+  v.addEventListener("click", handleClick, false);
   v.addEventListener("click", handleClick);
 });
 const first = document.querySelector(".first");
@@ -8,8 +8,26 @@ first.removeEventListener("click", handleClick);
 first.removeEventListener("click", handleClick, true);
 let i = 1;
 function handleClick(event) {
+  event.preventDefault();
   console.log(`이벤트 발생 순서: ${i++}`);
   console.log(`이벤트 타겟: ${event.target.className}`);
   console.log(`이벤트 currentTarget : ${event.currentTarget.className}`);
   console.log("-------------------------------------");
+}
+
+const a = document.querySelector("a");
+a.addEventListener("contextmenu", handler);
+document.addEventListener("contextmenu", documentContext);
+function handler(event) {
+  event.preventDefault();
+  console.log("click handler");
+}
+
+function documentContext(event) {
+  if (event.defaultPrevented) return;
+  else {
+    event.preventDefault();
+    console.log(event.defaultPrevented);
+    console.log("document");
+  }
 }
