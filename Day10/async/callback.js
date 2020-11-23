55edba0374785fd730e4428f41bb6bcb293bb064
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-11-23 22:05:33 +0900

    docs. Event Loop 다른 예제

--- Day10/async/callback.js
+++ Day10/async/callback.js
@@ -36,3 +36,19 @@
 // console.log(1);
 // setTimeout(() => console.log(2), 1000);
 // console.log(3);
+
+const baseData = [1, 2, 3, 4, 5, 6, 100];
+
+const asyncRun = (arr, fn) => {
+  arr.forEach((v, i) => {
+    setTimeout(() => {
+      setTimeout(() => {
+        console.log("cb 2");
+        fn(i);
+      }, 1000);
+      console.log("cb 1");
+    }, 1000);
+  });
+};
+
+asyncRun(baseData, (idx) => console.log(idx));
