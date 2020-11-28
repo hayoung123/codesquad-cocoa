d864fd7ad34b1ab3eeb71dd1d95b0f7a5e15fbb9
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-11-28 11:07:39 +0900

    algorithm. merge two list

--- Day12/README.md
+++ Day12/README.md
@@ -4,4 +4,13 @@
 [reverse-integer](https://leetcode.com/problems/reverse-integer/submissions/)
 [roman-to-integer](https://leetcode.com/problems/roman-to-integer/)
 [longest-common-prefix](https://leetcode.com/problems/longest-common-prefix/)
-[merge-two-sorted-lists](https://leetcode.com/problems/merge-two-sorted-lists/)
+
+## [merge-two-sorted-lists](https://leetcode.com/problems/merge-two-sorted-lists/)
+
+처음에 list형태여서 concat으로 붙이면 될 것이라 생각했는데 눈에 보기에만 list였을 뿐이고 실제로는 객체였다.
+
+1. `let listNode = new ListNode()`생성 & 변수 head를 만들어 Listnode를 참조해 놓고 마지막 출력에서 이용
+2. l1,l2가 null이 아닐때 돈다. 크기 비교 후 new ListNode(~~.val) 로 새로운 객체를 만들어 `listNode.next`에 추가
+3. l1 또는 l2 , listNode.next 다시 설정
+4. 둘 중 하나는 길이가 남아있을 것이기 때문에 마지막에 뒤에 다 붙여준다.
+5. 처음 참조했던 head.next를 반환
--- Day12/merge-two-list.js
+++ Day12/merge-two-list.js
@@ -1,8 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 const mergeTwoLists = (l1, l2) => {
-  return l1.concat(l2).sort((a, b) => a - b);
-};
+  let linkedList = new ListNode();
+  const head = linkedList;
 
-const l1 = [];
-const l2 = [0];
-const k = mergeTwoLists(l1, l2);
-console.log(k);
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      linkedList.next = new ListNode(l1.val);
+      l1 = l1.next;
+    } else {
+      linkedList.next = new ListNode(l2.val);
+      l2 = l2.next;
+    }
+    linkedList = linkedList.next;
+  }
+  if (l1 !== null) linkedList.next = l1;
+  else linkedList.next = l2;
+
+  return head.next;
+};
