95e3c3acea19e5603185672667dea5b416d4adbc
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-12-03 23:57:19 +0900

    refacor. colorList 사용하는 메소드 1개에서만 선언, render-block 함수에 shape data에 color이용

--- Final Day/scripts/tetris-view.js
+++ Final Day/scripts/tetris-view.js
@@ -18,17 +18,18 @@ export class TetrisView {
     this.tetrisModel = tetrisModel;
     this.model = tetrisModel.getModel();
     this.shapeList = shapeView.getShapeList();
-    this.colorList = shapeView.getColor();
     this.scoreLevelView = scoreLevelView;
     this.shape;
     this.nextShape;
     this.changeCnt = 0;
     this.cellSize = 30;
+    //메소드에서 class 생성 때 받은 파라미터를 못쓰기 때문에 따로 저장
     this.START_POINT = START_POINT;
     this.startLeft = START_POINT.LEFT;
     this.startTop = START_POINT.TOP;
     this.timer = null;
     this.requestID = null;
+    //removeEvent때 콜백함수가 다르면 안되기 때문에 따로 저장
     this.handleKeydown = this.handleKeydown.bind(this);
   }
 
@@ -38,6 +39,7 @@ export class TetrisView {
     this.playBtn.addEventListener("click", this.handleClick.bind(this));
     this.resetBtn.addEventListener("click", this.handleClick.bind(this));
   }
+  //random으로 next 블록 설정
   setNextShape() {
     const random = Math.floor(Math.random() * 7);
     this.nextShape = this.shapeList[random];
@@ -69,13 +71,13 @@ export class TetrisView {
     this.startLeft = this.START_POINT.LEFT;
     this.startTop = this.START_POINT.TOP;
   }
-  //새로운 블럭을 play하는것.
+  //새로운 블럭을 play
   play() {
     this.resetBlock();
     this.shape = this.nextShape;
     this.setNextShape();
-    this.renderNextBlock(this.colorList[this.nextShape.id]);
-    this.renderBlock(this.colorList[this.shape.id]);
+    this.renderNextBlock(this.nextShape.color);
+    this.renderBlock(this.shape.color);
     if (this.checkGameOver()) {
       return this.finishPlay();
     }
@@ -102,7 +104,7 @@ export class TetrisView {
     if (this.checkBlock(this.startLeft, this.startTop + this.cellSize)) {
       this.clear();
       this.startTop += this.cellSize;
-      this.renderBlock(this.colorList[this.shape.id]);
+      this.renderBlock(this.shape.color);
       this.timer = setTimeout(this.autoMove.bind(this), moveFast);
     } else {
       this.fixBlock();
@@ -156,7 +158,7 @@ export class TetrisView {
     } else if (keyCode === this.key.RIGHT) {
       this.startLeft += this.cellSize;
     }
-    this.renderBlock(this.colorList[this.shape.id]);
+    this.renderBlock(this.shape.color);
   }
 
   //space바 누를 시 drop
@@ -164,7 +166,7 @@ export class TetrisView {
     if (this.checkBlock(this.startLeft, this.startTop + this.cellSize)) {
       this.clear();
       this.startTop += this.cellSize;
-      this.renderBlock(this.colorList[this.shape.id]);
+      this.renderBlock(this.shape.color);
       this.requestID = requestAnimationFrame(this.drop.bind(this));
     } else {
       this.fixBlock();
@@ -190,7 +192,7 @@ export class TetrisView {
         this.changeCnt--;
       }
     }
-    this.renderBlock(this.colorList[this.shape.id]);
+    this.renderBlock(this.shape.color);
   }
 
   //충돌 check하기
@@ -293,10 +295,11 @@ export class TetrisView {
 
   //model에 따라서 쌓인 블럭들 그리기
   reScreen() {
+    const colorList = shapeView.getColor();
     for (let i = 1; i < this.model.length; i++) {
       for (let j = 0; j < this.model[i].length; j++) {
         if (this.model[i][j] !== 0) {
-          const color = this.colorList[this.model[i][j]];
+          const color = colorList[this.model[i][j]];
           this.renderBox(
             this.context,
             j * this.cellSize,
